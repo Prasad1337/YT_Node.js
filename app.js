34634c68bcdd322bc450399ba4a48faa7f30f01f
Prasad1337 <prasad1337@gmail.com>
2015-10-04 11:04:21 +0800

    transitioned from Jade to EJS

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-
 var mongoose = require('mongoose');
+
+var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
